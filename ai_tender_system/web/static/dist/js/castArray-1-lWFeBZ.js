1f7c4823f9fa1a093dba26c064e037d345b27647
LvHe <lvhe@Lvhe2023MacAir.local>
2025-11-04 14:37:35 +0800

    build: 更新Vue前端构建产物
    更新内容：
    - 新增企业详情页（CompanyDetail）构建文件
    - 更新企业库管理页（CompanyLibrary）构建文件
    - 更新所有Vue组件的生产构建（包含最新代码变更）
    - 清理旧的构建产物，生成新的哈希文件
    
    此次构建包含了以下功能改进：
    - 企业库列表页的搜索、筛选、统计功能
    - 企业详情页及相关组件
    - 资质管理、人员管理等模块

--- ai_tender_system/web/static/dist/js/castArray-1-lWFeBZ.js
+++
@@ -1,1 +0,0 @@
-import{bs as r}from"./index.js";function n(){if(!arguments.length)return[];var n=arguments[0];return r(n)?n:[n]}export{n as c};
--- ai_tender_system/web/static/dist/js/strings-cNGkbWe_.js
+++
@@ -1,1 +0,0 @@
-import{cN as e}from"./index.js";const a=(e="")=>e.replace(/[|\\{}()[\]^$+*?.]/g,"\\$&").replace(/-/g,"\\x2d"),c=a=>e(a);export{c,a as e};
